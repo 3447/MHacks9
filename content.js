64b77c0f9a12e38c1ae376d24432de1476cd0e2f
Jason Bilas <jasonbilas@gmail.com>
2017-03-25 14:52:55 -0400

    moved parsing to its own method

--- content.js
+++ content.js
@@ -18,43 +18,46 @@ function startQuery(quer){
   var searchUrl = 'https://api.wolframalpha.com/v2/query' + '?appid=' + encodeURIComponent(appID) +
   '&input=' + quer + '&format=plaintext&output=JSON';
   var x = new XMLHttpRequest();
-//  x.responseType = 'json';
-  x.onload = function() {
-    var response = JSON.parse(x.response);
-    console.log(response);
-    if (!response || !response["queryresult"]["success"] || response["queryresult"]["error"] ){
-      console.log('Invalid search to Wolfram');
-      return;
-    }
-    var importantTitle = {"definition":null, "definitions":null, "synonym":null, "synonyms":null, "antonym":null, "antonyms":null,
-                          "pronounciation":null, "image":null, "basic movie information":null, "cast":null, "wikipedia summary":null,
-                          "basic series information":null, "latest trade":null, "chemical names and formulas":null,
-                          "administrative regions": null, "current weather":null, "unit conversions":null, "basic information":null,
-                          "notable facts":null, "location and owner":null, "basic properties":null};
-    var importantIDs = {"observancedate (country)":null, "notableeventfordate":null};
-
-    var pods = response["queryresult"]["pods"];
-    console.log(pods);
-
-    for(i = 0; i < pods.length; i++) {
-      lowercase = pods[i].title.toLowerCase();
-      if(lowercase === "wikipedia summary"){
-        importantTitle[lowercase] = getWikipediaSummary(quer);
-      }
-      else if(lowercase in importantTitle){
-        importantTitle[lowercase] = pods[i].subpods[0].plaintext;
-      }
-      else if(lowercase in importantIDs){
-        importantIDs[lowercase] = pods[i].subpods[0].plaintext;
-      }
-    };
-  };
+  //  x.responseType = 'json';
+  x.onload = pareseResponse(x)
   x.onerror = function(err) {
     console.log(err);
   };
   x.open('GET', searchUrl, true);
   x.send();
 }
+
+function pareseResponse(x){
+  var response = JSON.parse(x.response);
+  console.log(response);
+  if (!response || !response["queryresult"]["success"] || response["queryresult"]["error"] ){
+    console.log('Invalid search to Wolfram');
+    return;
+  }
+  var importantTitle = {"definition":null, "definitions":null, "synonym":null, "synonyms":null, "antonym":null, "antonyms":null,
+                        "pronounciation":null, "image":null, "basic movie information":null, "cast":null, "wikipedia summary":null,
+                        "basic series information":null, "latest trade":null, "chemical names and formulas":null,
+                        "administrative regions": null, "current weather":null, "unit conversions":null, "basic information":null,
+                        "notable facts":null, "location and owner":null, "basic properties":null};
+  var importantIDs = {"observancedate (country)":null, "notableeventfordate":null};
+
+  var pods = response["queryresult"]["pods"];
+  console.log(pods);
+
+  for(i = 0; i < pods.length; i++) {
+    lowercase = pods[i].title.toLowerCase();
+    if(lowercase === "wikipedia summary"){
+      importantTitle[lowercase] = getWikipediaSummary(quer);
+    }
+    else if(lowercase in importantTitle){
+      importantTitle[lowercase] = pods[i].subpods[0].plaintext;
+    }
+    else if(lowercase in importantIDs){
+      importantIDs[lowercase] = pods[i].subpods[0].plaintext;
+    }
+  };
+}
+
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   console.log("Message recieved");
